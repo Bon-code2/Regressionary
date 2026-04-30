1a8fc0a221109b4bcbe9617595a6c62e25d306dd
LISBON <lisbon.official.2022@gmail.com>
2026-04-30 23:50:53 +0530

    paranthesis

--- app.py
+++ app.py
@@ -683,4 +683,4 @@ def timeseries():
 if __name__ == "__main__":
     # Get the port from the environment, default to 5000 for local testing
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port
+    app.run(host='0.0.0.0', port=port)
