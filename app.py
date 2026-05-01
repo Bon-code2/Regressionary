19681c0d374471c8a4b69d1fe0f6742fbf068992
LISBON <lisbon.official.2022@gmail.com>
2026-05-01 13:56:49 +0530

    Portbinding

--- app.py
+++ app.py
@@ -688,4 +688,5 @@ def timeseries():
     return render_template('timeseries.html', columns=columns, diagnostics=diagnostics, plot_html=plot_html)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
